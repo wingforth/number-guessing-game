51a46a57ed0c9846bbd89606efbbb2daf528bb0c
wingforth <bothspace@gmail.com>
2025-07-24 23:37:23 +0800

    refactor: modify the printed content

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -11,29 +11,28 @@ def _input_int(message: str = "", bound: tuple[int, int] | None = None) -> int:
         try:
             integer = int(s)
         except ValueError:
-            print(f"Invalid input, {s} is not an integer.")
+            print("Invalid input. Please enter a valid number.")
             continue
         if bound and not bound[0] <= integer <= bound[1]:
-            print(f"Invalid input, {integer} isn't in the range {list(bound)} (including both end points).")
+            print(f"Invalid input. Please enter a number in the range {list(bound)} (including both end points).")
             continue
         return integer
 
 
 def set_difficulty_level() -> int:
-    print("Please select the difficulty level:")
+    print("\nPlease select the difficulty level:")
     for i, (level, chances) in enumerate(DIFFICULTY, 1):
         print(f"{i}. {level} ({chances} chances)")
     print()
     choice = _input_int("Enter your choice: ", (1, len(DIFFICULTY)))
     level, chances = DIFFICULTY[choice - 1]
     print()
-    print(
-        f"Great! You have selected the {level} difficulty level. You have {chances} chances.\nLet's start the game!\n"
-    )
+    print(f"Great! You have selected the {level} difficulty level. You have {chances} chances.\n")
     return chances
 
 
 def guess(secret: int, chances: int) -> None:
+    print("Let's start the game!\n")
     for i in range(chances):
         answer = _input_int("Enter your guess: ")
         if answer == secret:
@@ -51,7 +50,7 @@ def play_game() -> None:
     print(
         "Welcome to the Number Guessing Game!\n"
         "I'm thinking of a number between 1 and 100.\n"
-        "You have several chances to guess the correct number.\n"
+        "You have several chances to guess the correct number."
     )
     chances = set_difficulty_level()
     secret_number = random.randint(1, 100)
